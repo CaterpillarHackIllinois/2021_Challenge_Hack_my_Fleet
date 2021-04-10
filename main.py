643dca4d272ea7ed2f9ca83a43e1e15f4f4d69db
nikz7 <71370024+nikz7@users.noreply.github.com>
2021-04-10 01:23:53 -0500

    Flask Structure

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
 print("HELLO HACKATHON")
+
+@app.route('/')
+def upload_file():
+   return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+

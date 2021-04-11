772097f8e9eb2b1a608d323ea6b1be98d29ee704
Memhir-Yasue <eyasu.woldu@temple.edu>
2021-04-11 11:46:42 -0400

    graphing methods will be stored here

---
+++ ceramic/graphlib.py
@@ -0,0 +1,21 @@
+import plotly.express as px
+
+def map_graph(df):
+
+    fig = px.scatter_mapbox(df, lat="GPS Lattitude_y", lon="GPS Longitude_y", hover_name="cluster_label",
+                            hover_data=["AssetID"], color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+
+    fig.update_layout(
+        mapbox_style="white-bg",
+        mapbox_layers=[
+            {
+                "below": 'traces',
+                "sourcetype": "raster",
+                "sourceattribution": "United States Geological Survey",
+                "source": [
+                    "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
+                ]
+            },
+        ])
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    return fig

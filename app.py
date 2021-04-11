6d987b632bf58aa1f6621eac14f6eb71dc249f06
rushatgabhane <rushatgabhane@gmail.com>
2021-04-12 00:34:42 +0300

    styling done

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ import dash
 import dash_core_components as dcc
 import dash_table
 import dash_html_components as html
+import dash_daq as daq
 import pandas as pd
 from graphlib import map_graph
 import os
 
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# app = dash.Dash(__name__)
 
 # deployment heroku
 server = app.server
@@ -33,43 +34,63 @@ app.layout = html.Div([
 
     html.Div([
         html.Img(src = "assets/images/logo.png", id = "logo"),
-        html.H1('FUEL ANALYTICS', className="app-header"),
+        html.H1('FUEL ANALYTICS', className="app-header padding-l-20"),
 
         # html.Div(children='''
         # A dashboard for fleet fuel management and optimization!
         # '''),
         html.Div([
-            html.Div(["Gender: ", dcc.Dropdown(id='gender',
-                                            options=[{'label': k, 'value': v} for k, v in genders.items()],
-                                            value=1)]),
-            html.Br(),
-            html.Div(["Highest Education: ", dcc.Dropdown(id='education',
-                                                        options=[{'label': k, 'value': v} for k, v in educations.items()],
-                                                        value=2, )]),
-            html.Br(),
-            html.Div(["Age: ", dcc.Dropdown(id='age',
-                                            options=[{'label': k, 'value': v} for k, v in ages.items()],
-                                            value=0, )]),
-            html.Br(),
-            html.Div(["Socio-Economic-Range: ", dcc.Dropdown(id='soecon',
-                                                            options=[{'label': k, 'value': v} for k, v in socio.items()],
-                                                            value=55.0, )]),
-            html.Br(),
+            # html.Div(["Gender: ", dcc.Dropdown(id='gender',
+            #                                 options=[{'label': k, 'value': v} for k, v in genders.items()],
+            #                                 value=1)]),
+            # html.Br(),
+            # html.Div(["Highest Education: ", dcc.Dropdown(id='education',
+            #                                             options=[{'label': k, 'value': v} for k, v in educations.items()],
+            #                                             value=2, )]),
+            # html.Br(),
+            # html.Div(["Age: ", dcc.Dropdown(id='age',
+            #                                 options=[{'label': k, 'value': v} for k, v in ages.items()],
+            #                                 value=0, )]),
+            # html.Br(),
+            # html.Div(["Socio-Economic-Range: ", dcc.Dropdown(id='soecon',
+            #                                                 options=[{'label': k, 'value': v} for k, v in socio.items()],
+            #                                                 value=55.0, )]),
+            # html.Br(),
+            daq.ToggleSwitch(
+                id='toggle-1',
+                className='toggle',
+                value=False,
+                labelPosition = "bottom",
+                label = "Monthly | Average",
+            ),
+             daq.ToggleSwitch(
+                id='toggle-2',
+                className='toggle',
+                value=False,
+                labelPosition = "bottom",
+                label = "Cluster Aggregation",
+            ),
+             daq.ToggleSwitch(
+                id='toggle-3',
+                className='toggle',
+                value=False,
+                labelPosition = "bottom",
+                label = "Offender or general",
+            ),
+             daq.ToggleSwitch(
+                id='toggle-4',
+                className='toggle',
+                value=False,
+                labelPosition = "bottom",
+                label = "Weather",
+            ),
             html.Div(
-                ["Num of Prev Attempts (for course you'll be taking): ", dcc.Input(id='attempt', value=0, type='number')]),
+                ["K clusters ", dcc.Input(id='k-cluster', value=0, type='number')], id='css-low-fuel'),
+            html.Div(
+                ["Low fuel threshold % ", dcc.Input(id='low-fuel', value=0, type='number')], style={'paddingTop': '20px'}),
             html.Br(),
-            html.Div(["Credits (30-300): ", dcc.Input(id='credit', value='30', type='number')]),
+            html.Div(["Fuel Price: ", dcc.Input(id='fuel-price', value='30', type='number')]),
             html.Br(),
-            html.Div(["Disability: ", dcc.Dropdown(id='disability',
-                                                options=[{'label': k, 'value': v} for k, v in disability.items()],
-                                                value=0)]),
-            html.Br(),
-            html.Div(["How Productive are you?: ", dcc.Dropdown(id='productivity',
-                                                                options=[{'label': k, 'value': v} for k, v in
-                                                                        productivity.items()],
-                                                                value=170)]),
-            html.Br(),
-            html.H6(children=[html.Div(id='my-output', )])
         ], id='control-panel')
     ], id='side-bar'),
 
